3a8a973545662768e2cbb12ef3cc4d4bdff23308
makebreak <andystudent0@gmail.com>
2019-03-31 00:21:23 -0400

    glass falling edits

--- GlassFalling.java
+++ GlassFalling.java
@@ -5,6 +5,7 @@
 public class GlassFalling {
 
   // Do not change the parameters!
+
     public int glassFallingRecur(int floors, int sheets) {
 
 	// would only need 1 trial if there is 1 floor, none if none
@@ -18,43 +19,46 @@ public class GlassFalling {
 	if (sheets == 1)
 	    return floors;
 
-	int min = Integer.MAX_VALUE;
-
+	int min = Integer.MAX_VALUE; // set min to a high number, to tell if it didn't change
 	int x, y;
 
-	// the lower part of the building
+	// as we go up the floors, 
 	for (x = 1; x <= floors; x++) {
-	    y = Math.max(glassFallingRecur(sheets-1, x-1),
-			 glassFallingRecur(sheets, floors-x));
+	    y = Math.max(glassFallingRecur(x-1, sheets-1), //check lower floors, assume sheet broke
+			 glassFallingRecur(floors-x, sheets)); //check upper floors, assume sheet okay
 	    if (y < min)
-		min = y;
+		min = y; // set min to value above
+	    System.out.println("This x is " + x + "with a min of " + min);
 	}
-
 	
-	return min + 1;
+       	return min + 1; // we add 1 to show we made it through another trial
   }
 
     public boolean tryGlass(int n) {
 	if (n >= 3) return true;
 	else return false;
     }
-  // Optional:
-  // Pick whatever parameters you want to, just make sure to return an int.
-	public int glassFallingMemoized(int floors, int sheets, int low, int high) {
-    // Fill in here and change the return
-    return 0;
-  }
+
+    // Optional:
+    // Pick whatever parameters you want to, just make sure to return an int.
+    public int glassFallingMemoized(int floors, int sheets, int low, int high, int dp[]) {
+
+	// Fill in here and change the return
+	return 0;
+    }
+
+
 
   // Do not change the parameters!
   public int glassFallingBottomUp(int floors, int sheets) {
-    // Fill in here and change the return
+      int[] dp = new int[floors];
     return 0;
   }
 
 
   public static void main(String args[]){
       GlassFalling gf = new GlassFalling();
-
+      /*
       // Do not touch the below lines of code, and make sure
       // in your final turned-in copy, these are the only things printed
       int minTrials1Recur = gf.glassFallingRecur(27, 2);
@@ -63,5 +67,15 @@ public class GlassFalling {
       int minTrials2Bottom = gf.glassFallingBottomUp(100, 3);
       System.out.println(minTrials1Recur + " " + minTrials1Bottom);
       System.out.println(minTrials2Recur + " " + minTrials2Bottom);
+      */
+
+      //DELETE LATER
+      int minTrials1Recur = gf.glassFallingRecur(10, 2);
+      int minTrials1Bottom = gf.glassFallingBottomUp(10, 2);
+      int minTrials2Recur = gf.glassFallingRecur(4, 2);
+      int minTrials2Bottom = gf.glassFallingBottomUp(4, 2);
+      System.out.println(minTrials1Recur + " " + minTrials1Bottom);
+      System.out.println(minTrials2Recur + " " + minTrials2Bottom);
+
   }
 }
